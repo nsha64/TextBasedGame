f384c7db5254e4b944f39ff10f461c249559b391
nsha64 <neelstrong@outlook.com>
2020-06-07 03:30:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+go = input('Would you like to play the maze game: ')
+
+def Main():
+  name = input('What would you like me to call you: ')
+  health = 10
+  print('You currently have 10 health, get through the maze with losing all your health to win!')
+  print("Let's get started!")
+  opt1 = input('Would you like to go left or right: ')
+  if opt1 == 'left' | 'Left':
+    print('Good job, keep going')
+  else:
+    print("Oops, you took a wrong turn, you died")
+  opt2 = input('Would you like to go left or right: ')
+  
+
+
+if go == 'yes':
+  Main()
+elif go == 'no':
+  pass
+else:
+  Main()
